2542701ecd545c28216f6e303c9d4d0bbfaae3a9
jingjingxyk <zonghengbaihe521@qq.com>
2022-12-06 04:31:40 +0800

    save changed

--- build-init-depend.sh
+++ build-init-depend.sh
@@ -7,8 +7,18 @@ __DIR__=$(
 )
 cd ${__DIR__}
 
-export http_proxy=http://192.168.3.26:8015
-export https_proxy=http://192.168.3.26:8015
+PROXY_URL=${2:+'http://127.0.0.1:8015'}
+
+if test -n $PROXY_URL
+then
+  export http_proxy=http://192.168.3.26:8015
+  export https_proxy=http://192.168.3.26:8015
+fi
+
+
 pear config-set http_proxy $http_proxy
-SKIP_LIBRARY_DOWNLOAD=1 php prepare.php +mongodb +inotify
+
+# SKIP_LIBRARY_DOWNLOAD=1 php prepare.php +mongodb +inotify
+php prepare.php  +inotify
+
 chmod a+x ./make.sh
