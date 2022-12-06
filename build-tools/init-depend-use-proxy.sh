ec868246294930c43520733b17c12b12f5473264
jingjingxyk <zonghengbaihe521@qq.com>
2022-12-07 00:57:57 +0800

    save changed

--- build-tools/init-depend-use-proxy.sh
+++ build-tools/init-depend-use-proxy.sh
@@ -8,6 +8,11 @@ __DIR__=$(
 cd ${__DIR__}
 cd ${__DIR__}/../
 
+export http_proxy=http://192.168.3.26:8015
+export https_proxy=http://192.168.3.26:8015
+
+pear config-set http_proxy $http_proxy
+
 # SKIP_LIBRARY_DOWNLOAD=1 php prepare.php +mongodb +inotify
 php prepare.php  +inotify
 
