c302e6f951d41e28a3a9194143203a773bbc143a
jingjingxyk <zonghengbaihe521@qq.com>
2022-12-07 00:19:30 +0800

    修改目录忽略

--- build-tools/download-php-source-code.sh
+++ build-tools/download-php-source-code.sh
@@ -10,3 +10,6 @@ cd ${__DIR__}/php-versions
 
 curl -Lo php-8.1.12.tar.gz https://www.php.net/distributions/php-8.1.12.tar.gz
 tar -zxvf php-8.1.12.tar.gz
+
+
+curl -Lo xz-5.2.9.tar.gz https://tukaani.org/xz/xz-5.2.9.tar.gz
