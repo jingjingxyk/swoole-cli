75cf852d96934d9b522b95fc6df0d3195063211c
jingjingxyk <zonghengbaihe521@qq.com>
2022-12-06 10:37:52 +0800

    save changed

--- build-tools/prepare-run-container.sh
+++ build-tools/prepare-run-container.sh
@@ -16,25 +16,21 @@ test -d ${__ROOT__}/build-tools/dist/ && sudo rm -rf ${__ROOT__}/build-tools/dis
 mkdir -p ${__ROOT__}/build-tools/dist/
 
 rsync -avr --delete-before --stats --progress ${__ROOT__}/ ${__ROOT__}/build-tools/dist/ \
---exclude  ${__ROOT__}/build-tools/dist \
---exclude  ${__ROOT__}/Dockerfile-old \
---exclude  ${__ROOT__}/Dockerfile \
---exclude  ${__ROOT__}/README.md \
---exclude  ${__ROOT__}/.github \
---exclude  ${__ROOT__}/.idea
-
+  --exclude ${__ROOT__}/build-tools/dist \
+  --exclude ${__ROOT__}/Dockerfile-old \
+  --exclude ${__ROOT__}/Dockerfile \
+  --exclude ${__ROOT__}/README.md \
+  --exclude ${__ROOT__}/.github \
+  --exclude ${__ROOT__}/.idea
 
 # docker run --rm --name swoole-cli-build-dev -v ${__DIR__}/dist:/work -w /work -ti --init  docker.io/jingjingxyk/build-swoole-cli:alpine-edge-20221205T144525Z
 
 {
-  docker stop swoole-cli-build-dev && docker rm swoole-cli-build-dev
+  docker stop swoole-cli-build-dev
+  docker rm swoole-cli-build-dev
 } || {
   echo $?
 }
 
-
 image=$(cat base-container-image.txt)
-docker run --rm --name swoole-cli-build-dev  -d -v ${__DIR__}/dist:/work -w /work  $image  tail -f /dev/null
-
-
-
+docker run --rm --name swoole-cli-build-dev -d -v ${__DIR__}/dist:/work -w /work $image tail -f /dev/null
