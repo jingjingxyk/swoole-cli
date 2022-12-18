89bb2ec04796b77f410bccbaaa40b9d97fa6a177
jingjingxyk <zonghengbaihe521@qq.com>
2022-12-18 15:17:27 +0800

    save

--- conf.d/swoole.php
+++ conf.d/swoole.php
@@ -5,8 +5,10 @@ use SwooleCli\Extension;
 
 return function (Preprocessor $p) {
     $p->addExtension((new Extension('swoole'))
-        ->withOptions('--enable-swoole --enable-sockets --enable-mysqlnd --enable-swoole-curl --enable-cares --enable-swoole-pgsql --with-openssl-dir=/usr/openssl  ' .
+        ->withOptions('--enable-swoole --enable-sockets --enable-mysqlnd --enable-swoole-curl --enable-cares  --with-openssl-dir=/usr/openssl  ' .
             '--with-brotli-dir=/usr/brotli')
+        //->withOptions('--enable-swoole --enable-sockets --enable-mysqlnd --enable-swoole-curl --enable-cares --enable-swoole-pgsql --with-openssl-dir=/usr/openssl  ' .
+        //    '--with-brotli-dir=/usr/brotli')
         ->withLicense('https://github.com/swoole/swoole-src/blob/master/LICENSE', Extension::LICENSE_APACHE2)
         ->withHomePage('https://github.com/swoole/swoole-src')
     );
