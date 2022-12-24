d55058993fd900fc429f5482a3dcd04512b9d3f1
jingjingxyk <zonghengbaihe521@qq.com>
2022-12-24 19:43:16 +0800

    save changed

--- sapi/make.php
+++ sapi/make.php
@@ -164,6 +164,7 @@ elif [ "$1" = "sync" ] ;then
   cp -r $SRC/ext/pcre/ ./ext
   cp -r $SRC/ext/pdo/ ./ext
   cp -r $SRC/ext/pdo_mysql/ ./ext
+  cp -r $SRC/ext/pdo_pgsql/ ./ext
   cp -r $SRC/ext/pdo_sqlite/ ./ext
   cp -r $SRC/ext/phar/ ./ext
   cp -r $SRC/ext/posix/ ./ext
@@ -175,6 +176,7 @@ elif [ "$1" = "sync" ] ;then
   cp -r $SRC/ext/sockets/ ./ext
   cp -r $SRC/ext/sodium/ ./ext
   cp -r $SRC/ext/spl/ ./ext
+  cp -r $SRC/ext/skeleton/ ./ext
   cp -r $SRC/ext/sqlite3/ ./ext
   cp -r $SRC/ext/standard/ ./ext
   cp -r $SRC/ext/sysvshm/ ./ext
