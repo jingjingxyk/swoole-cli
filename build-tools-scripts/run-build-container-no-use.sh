ef691125b7edc017c6bf1cccc6f4505f015ababd
jingjingxyk <zonghengbaihe521@qq.com>
2022-12-25 14:56:12 +0800

    save changed

--- build-tools-scripts/run-build-container-no-use.sh
+++ build-tools-scripts/run-build-container-no-use.sh
@@ -5,22 +5,11 @@ __DIR__=$(
   cd "$(dirname "$0")"
   pwd
 )
-cd ${__DIR__}
-
-__ROOT__=$(
+__PROJECT__=$(
   cd ${__DIR__}/../
   pwd
 )
-
-{
-  docker stop swoole-cli-build-dev
-  docker rm swoole-cli-build-dev
-} || {
-  echo $?
-}
-
-image=$(cat base-container-image.txt)
-docker run --rm --name swoole-cli-build-dev -d -v ${__ROOT__}:/work -w /work $image tail -f /dev/null
+cd ${__DIR__}
 
 
 exit 0
