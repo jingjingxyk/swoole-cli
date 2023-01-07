337ab29c94c1fd533a48e3192f3c17b1e49f9443
jingjingxyk <zonghengbaihe521@qq.com>
2023-01-07 17:48:05 +0800

    save changed

--- build-tools-scripts/run-swoole-cli-build-dev-2-container.sh
+++ build-tools-scripts/run-swoole-cli-build-dev-2-container.sh
@@ -25,9 +25,7 @@ cd ${__DIR__}
 
 cd ${__DIR__}
 test -f swoole-cli-build-dev-2-container.txt && image=$(cat swoole-cli-build-dev-2-container.txt)
-test -f swoole-cli-build-dev-2-container.txt || image=docker.io/jingjingxyk/build-swoole-cli:build-dev-2-alpine-edge-20221228T065248Z
-
-
+test -f swoole-cli-build-dev-2-container.txt || image=docker.io/jingjingxyk/build-swoole-cli:build-dev-all-dependencies-alpine-edge-20230107T091604Z
 cd ${__DIR__}
 
 
