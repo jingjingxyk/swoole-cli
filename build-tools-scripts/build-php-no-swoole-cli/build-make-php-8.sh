d2ae81630e02e30276080b8335c4313e71a5bd97
jingjingxyk <zonghengbaihe521@qq.com>
2023-02-06 18:14:50 +0800

    swoole upgrade to v5.0.2

--- build-tools-scripts/build-php-no-swoole-cli/build-make-php-8.sh
+++ build-tools-scripts/build-php-no-swoole-cli/build-make-php-8.sh
@@ -14,7 +14,7 @@ cd ${__DIR__}
 cd ${__DIR__}/php-src/
 
 # make clean
-make     EXTRA_LDFLAGS_PROGRAM='/usr/libiconv/lib/pkgconfig:/usr/openssl/lib64/pkgconfig:/usr/libxml2/lib/pkgconfig:/usr/libxslt/lib/pkgconfig:/usr/gmp/lib/pkgconfig:/usr/zlib/lib/pkgconfig:/usr/liblz4/lib/pkgconfig:/usr/zip/lib/pkgconfig:/usr/libpng/lib/pkgconfig:/usr/libjpeg/lib64/pkgconfig:/usr/brotli/lib/pkgconfig:/usr/libwebp/lib/pkgconfig:/usr/freetype/lib/pkgconfig:/usr/sqlite3/lib/pkgconfig:/usr/oniguruma/lib/pkgconfig:/usr/c-ares/lib/pkgconfig:/usr/imagemagick/lib/pkgconfig:/usr/curl/lib/pkgconfig:/usr/libsodium/lib/pkgconfig:/usr/libyaml/lib/pkgconfig:/usr/mimalloc/lib/pkgconfig:/usr/pgsql/lib/pkgconfig'   -j  $(nproc)
+make     EXTRA_LDFLAGS_PROGRAM='/usr/libiconv/lib/pkgconfig:/usr/openssl_3/lib64/pkgconfig:/usr/libxml2/lib/pkgconfig:/usr/libxslt/lib/pkgconfig:/usr/gmp/lib/pkgconfig:/usr/zlib/lib/pkgconfig:/usr/liblz4/lib/pkgconfig:/usr/zip/lib/pkgconfig:/usr/libpng/lib/pkgconfig:/usr/libjpeg/lib64/pkgconfig:/usr/brotli/lib/pkgconfig:/usr/libwebp/lib/pkgconfig:/usr/freetype/lib/pkgconfig:/usr/sqlite3/lib/pkgconfig:/usr/oniguruma/lib/pkgconfig:/usr/c-ares/lib/pkgconfig:/usr/imagemagick/lib/pkgconfig:/usr/curl/lib/pkgconfig:/usr/libsodium/lib/pkgconfig:/usr/libyaml/lib/pkgconfig:/usr/mimalloc/lib/pkgconfig:/usr/pgsql/lib/pkgconfig'   -j  $(nproc)
 
 
 
