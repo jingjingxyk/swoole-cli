869efad0a2d9ee5b226a976e391644f6d1320daf
jingjingxyk <zonghengbaihe521@qq.com>
2023-05-11 22:36:15 +0800

    save changed

--- sapi/dashboard/bootstrap.php
+++ sapi/dashboard/bootstrap.php
@@ -9,19 +9,16 @@ use function Swoole\Coroutine\run;
 use Swoole\Coroutine\Channel;
 use Swoole\Coroutine;
 
-
-
-
 run(function () {
     $server = new Server('0.0.0.0', 9502, false);
 
     $channel = new Channel(1);
 
-    $server->handle('/', function ($request, $response) use($server){
+    $server->handle('/', function ($request, $response) use ($server) {
         var_dump($request->server);
         $response->end('<h1>hello </h1>');
     });
-    $server->handle('/api', function (Request $request, Response $response) use ($server,$channel) {
+    $server->handle('/api', function (Request $request, Response $response) use ($server, $channel) {
         $response->header('Content-Type', 'application/json; charset=utf-8');
         $response->header('access-control-allow-credentials', 'true');
         $response->header('access-control-allow-methods', 'GET,HEAD,POST,OPTIONS');
@@ -46,25 +43,44 @@ run(function () {
         $action = lcfirst($action) . 'Action';
 
         var_dump($action);
-
+        $parameter= $request->getContent();
+        $parameter=json_decode($parameter, true);
+        var_dump($parameter);
+        $branch_name = $parameter['data']['branch_name'];
         $word_dir=realpath(__DIR__ . '/../../');
         $runtime=realpath($word_dir . '/bin/runtime');
+        if ($action==='changeBranchAction') {
+            $cmd=<<<EOF
+         cd $word_dir
+         git checkout $branch_name
+
+EOF;
+            ob_start();
+            passthru($cmd,$result_code);
+            $result = ob_get_contents();
+            ob_end_clean();
+            echo $result;
+        }
+
         $cmd=<<<EOF
         cd $word_dir
         export PATH=${runtime}:\$PATH
         php prepare.php --with-build-type=release +apcu +ds
 
 EOF;
-        ob_start();
-         //passthru($cmd,$result_code);
-        $result = ob_get_contents();
-        ob_end_clean();
-        echo $result;
+
 
 
         try {
-
-            $response->end( $result);
+            $response->end(json_encode(
+                [
+                'code'=>200,
+                "msg"=>'success',
+                "data"=>[
+                    'result'=>$result
+                ]],
+                JSON_UNESCAPED_UNICODE
+            ));
         } catch (\RuntimeException $e) {
             echo $e->getMessage();
             $response->end(json_encode(["code" => 500, 'msg' => 'system error' . $e->getMessage()]));
