3ff7ae1fedd7c0f57489674463290aecd267d57f
jingjingxyk <zonghengbaihe521@qq.com>
2023-05-20 01:08:41 +0800

    update

--- conf.d/swoole.php
+++ conf.d/swoole.php
@@ -5,7 +5,7 @@ use SwooleCli\Preprocessor;
 use SwooleCli\Extension;
 
 return function (Preprocessor $p) {
-    $depends = ['curl', 'openssl', 'cares', 'zlib', 'brotli', 'nghttp2'];
+    $depends = ['curl', 'openssl', 'cares', 'zlib', 'brotli'];
 
     $options = '--enable-swoole --enable-sockets --enable-mysqlnd --enable-swoole-curl --enable-cares ';
     $options .= ' --with-brotli-dir=' . BROTLI_PREFIX;
@@ -15,19 +15,18 @@ return function (Preprocessor $p) {
         $options .= ' --enable-swoole-pgsql';
         $depends[] = 'pgsql';
     }
-    $ext=(new Extension('swoole'))
-            ->withOptions($options)
-            ->withLicense('https://github.com/swoole/swoole-src/blob/master/LICENSE', Extension::LICENSE_APACHE2)
-            ->withHomePage('https://github.com/swoole/swoole-src')
-            ->withFile('swoole-v4.8.x.tar.gz')
-            ->withDownloadScript(
-                'swoole-src',
-                <<<EOF
+    $ext = (new Extension('swoole'))
+        ->withOptions($options)
+        ->withLicense('https://github.com/swoole/swoole-src/blob/master/LICENSE', Extension::LICENSE_APACHE2)
+        ->withHomePage('https://github.com/swoole/swoole-src')
+        ->withFile('swoole-v4.8.x.tar.gz')
+        ->withDownloadScript(
+            'swoole-src',
+            <<<EOF
             git clone -b 4.8.x --depth=1  https://github.com/swoole/swoole-src.git
 EOF
-            );
+        );
 
-    call_user_func_array([$ext,'depends'], $depends);
+    call_user_func_array([$ext, 'depends'], $depends);
     $p->addExtension($ext);
-
 };
