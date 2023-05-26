7c9975a034bd9e906d870b5494b27d810955297b
jingjingxyk <zonghengbaihe521@qq.com>
2023-05-27 03:23:35 +0800

    save changed

--- sapi/src/builder/extension/intl.php
+++ sapi/src/builder/extension/intl.php
@@ -7,10 +7,30 @@ use SwooleCli\Extension;
 return function (Preprocessor $p) {
     $p->withExportVariable('ICU_CFLAGS', '$(pkg-config  --cflags --static icu-i18n  icu-io   icu-uc)');
     $p->withExportVariable('ICU_LIBS', '$(pkg-config    --libs   --static icu-i18n  icu-io   icu-uc)');
+
+
     $p->addExtension(
         (new Extension('intl'))
             ->withHomePage('https://www.php.net/intl')
             ->withOptions('--enable-intl')
             ->depends('icu')
     );
+    //解决 ICU 多重定义BUG https://bugs.php.net/bug.php?id=80425
+
+    $p->setExtHook('intl', function (Preprocessor $p) {
+        // compatible with redis
+        $workdir = $p->getWorkDir();
+
+        $cmd = <<<EOF
+                cd {$p->getPhpSrcDir()}/
+                if [[ ! -f ext/intl/msgformat/msgformat_helpers.cpp.backup ]] ;then
+                   sed -i.backup '67,71d' ext/intl/msgformat/msgformat_helpers.cpp
+                   echo "ok"
+                fi
+
+
+EOF;
+
+        return $cmd;
+    });
 };
