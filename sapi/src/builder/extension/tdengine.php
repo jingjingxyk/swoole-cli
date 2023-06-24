21900194fac792ef6035176bd71a74b3d05a07dd
jingjingxyk <zonghengbaihe521@qq.com>
2023-06-25 00:07:03 +0800

    添加tdengine 库

--- sapi/src/builder/extension/tdengine.php
+++ sapi/src/builder/extension/tdengine.php
@@ -10,17 +10,11 @@ return function (Preprocessor $p) {
 
     $ext = (new Extension('tdengine'))
         ->withOptions($options)
-        ->withLicense('https://github.com/swoole/swoole-src/blob/master/LICENSE', Extension::LICENSE_APACHE2)
+        ->withLicense('https://github.com/Yurunsoft/php-tdengine/blob/master/LICENSE', Extension::LICENSE_SPEC)
         ->withHomePage('https://github.com/Yurunsoft/php-tdengine.git')
         ->withManual('https://wiki.swoole.com/#/')
         ->withUrl('https://github.com/Yurunsoft/php-tdengine/archive/refs/tags/v1.0.6.tar.gz')
-        ->withFile('php-tdengine-v1.0.6.tar.gz')
-        ->withDownloadScript(
-            'php-tdengine',
-            <<<EOF
-         git clone -b v1.0.6 --dept=1 https://github.com/Yurunsoft/php-tdengine.git
-EOF
-        )
+        ->withFile('tdengine-v1.0.6.tar.gz')
         ->withDependentExtensions('swoole');
     $p->addExtension($ext);
 };
