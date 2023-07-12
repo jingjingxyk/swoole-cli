dfd37748fb0c53ad9d8828252f933306baa7ba0b
jingjingxyk <zonghengbaihe521@qq.com>
2023-07-13 06:04:28 +0800

    验证mongodb 扩展

--- sapi/src/builder/extension/mongodb.php
+++ sapi/src/builder/extension/mongodb.php
@@ -21,12 +21,12 @@ return function (Preprocessor $p) {
     $p->withExportVariable('PHP_MONGODB_ZLIB_CFLAGS', '$(pkg-config --cflags --static zlib)');
     $p->withExportVariable('PHP_MONGODB_ZLIB_LIBS', '$(pkg-config   --libs   --static zlib)');
 
-
     $options = ' --enable-mongodb ';
-    $options .= ' --with-mongodb-system-libs=yes ';
+    $options .= ' --with-mongodb-system-libs=no ';
     $options .= ' --with-mongodb-ssl=openssl ';
     $options .= ' --with-mongodb-sasl=no ';
     $options .= ' --with-mongodb-icu=yes ';
+    $options .= ' --with-mongodb-client-side-encryption=no ';
 
     $options .= ' --with-mongodb-snappy=no '; # v1.16 add parameter  https://github.com/mongodb/mongo-php-driver/issues/1427
     $options .= ' --with-mongodb-zlib=yes ';  # v1.16 add parameter
@@ -42,7 +42,7 @@ return function (Preprocessor $p) {
         ->withDependentExtensions('openssl')
     ;
 
-    $depends = ['icu', 'openssl', 'zlib', 'libzstd','bison','mongo_c_driver'];
+    $depends = ['icu', 'openssl', 'zlib', 'libzstd','bison']; // ,'mongo_c_driver','libbson'
 
     //$depends[] = 'libsasl';
     //$depends[] = 'snappy';
@@ -50,4 +50,18 @@ return function (Preprocessor $p) {
     call_user_func_array([$ext, 'withDependentLibraries'], $depends);
 
     $p->addExtension($ext);
+
+    $p->setExtHook('mongodb', function (Preprocessor $p) {
+
+        $workdir= $p->getPhpSrcDir();
+        # bug https://github.com/mongodb/mongo-c-driver/blob/6b7caf9da30eeae09c8eb0c539ebacbb31b9e520/src/libbson/src/bson/bson-error.c#L113
+        $cmd = <<<EOF
+                cd {$workdir}
+                # sed -i.backup "497c  "  ext/mongodb/config.m4
+                sed -i.backup "113c  "  ext/mongodb/src/libmongoc/src/libbson/src/bson/bson-error.c
+                sed -i.backup "113c  "  ext/mongodb/src/libmongoc/src/libbson/src/bson/bson-error.c
+EOF;
+
+        return $cmd;
+    });
 };
--- sapi/src/builder/library/mongo_c_driver.php
+++ sapi/src/builder/library/mongo_c_driver.php
@@ -12,45 +12,63 @@ return function (Preprocessor $p) {
     $zlib_prefix = ZLIB_PREFIX;
     $bzip2_prefix = BZIP2_PREFIX;
     $icu_prefix = ICU_PREFIX;
+    $bison_prefix = BISON_PREFIX;
+    $libbson_prefix = LIBBSON_PREFIX;
     $p->addLibrary(
         (new Library('mongo_c_driver'))
             ->withHomePage('https://www.mongodb.com/docs/drivers/c/')
             ->withLicense('https://github.com/mongodb/mongo-c-driver/blob/master/COPYING', Library::LICENSE_APACHE2)
             ->withManual('https://mongoc.org/libmongoc/current/tutorial.html')
+            ->withManual('https://www.mongoc.org/libmongoc/current/installing.html')
             ->withUrl('https://github.com/mongodb/mongo-c-driver/releases/download/1.24.1/mongo-c-driver-1.24.1.tar.gz')
             ->withFile('mongo-c-driver-1.24.1.tar.gz')
             ->withPrefix($mongo_c_driver_prefix)
             ->withCleanBuildDirectory()
-            ->withBuildScript(<<<EOF
+            ->withBuildScript(
+                <<<EOF
              mkdir -p cmake-build
             cd cmake-build
             # 查看配置选项
             # cmake -L ..
             cmake .. \
             -DCMAKE_INSTALL_PREFIX={$mongo_c_driver_prefix} \
-            -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF \
             -DCMAKE_POLICY_DEFAULT_CMP0074=NEW \
             -DCMAKE_BUILD_TYPE=Release \
             -DBUILD_SHARED_LIBS=OFF \
             -DBUILD_STATIC_LIBS=ON \
-
+             -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF \
             -Dlibzstd_ROOT={$libzstd_prefix} \
             -DOpenSSL_ROOT={$openssl_prefix} \
             -DZLIB_ROOT={$zlib_prefix} \
             -DICU_ROOT={$icu_prefix} \
-            -DUSE_SYSTEM_LIBBSON=OFF \
+            -DBSON_ROOT_DIR={$libbson_prefix} \
             -DENABLE_STATIC=ON \
             -DENABLE_SNAPPY=OFF \
             -DENABLE_ZSTD=ON \
             -DENABLE_ZLIB=SYSTEM \
             -DENABLE_SSL=OPENSSL \
             -DENABLE_SASL=OFF \
-            -DENABLE_ICU=ON
+            -DENABLE_ICU=ON \
+            -DENABLE_TESTS=OFF \
+            -DENABLE_MONGOC=ON \
+            -DUSE_SYSTEM_LIBBSON=ON \
+            -DENABLE_EXAMPLES=OFF \
+            -DMONGOC_ENABLE_STATIC_BUILD=ON \
+            -DMONGOC_ENABLE_STATIC_INSTALL=ON
 
 
             cmake --build . --target install
 EOF
             )
+            ->withScriptAfterInstall(
+                <<<EOF
+            rm -rf {$mongo_c_driver_prefix}/lib/*.so.*
+            rm -rf {$mongo_c_driver_prefix}/lib/*.so
+            rm -rf {$mongo_c_driver_prefix}/lib/*.dylib
+
+EOF
+            )
             ->withDependentLibraries('openssl', 'readline', 'zlib', 'libzstd', 'icu')
+            ->withPkgName('libbson-1.0')
     );
 };
