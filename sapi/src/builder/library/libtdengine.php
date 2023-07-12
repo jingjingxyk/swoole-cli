74e0d31955365435c74000d0d9adfe903d4297a3
jingjingxyk <zonghengbaihe521@qq.com>
2023-07-13 01:02:26 +0800

    update libtdengine

--- sapi/src/builder/library/libtdengine.php
+++ sapi/src/builder/library/libtdengine.php
@@ -10,10 +10,10 @@ return function (Preprocessor $p) {
             ->withHomePage('https://www.taosdata.com/')
             ->withLicense('https://github.com/taosdata/TDengine/blob/main/LICENSE', Library::LICENSE_SPEC)
             ->withManual('https://docs.taosdata.com/get-started/')
-            ->withUrl('https://github.com/taosdata/TDengine/archive/refs/tags/ver-3.0.5.1.tar.gz')
-            ->withUrl('https://docs.taosdata.com/releases/tdengine/#!')
+            ->withUrl('https://github.com/taosdata/TDengine/archive/refs/tags/ver-3.0.7.0.tar.gz')
+            ->withFile('TDengine-ver-3.0.7.0.tar.gz')
+           // ->withUrl('https://www.taosdata.com/assets-download/3.0/TDengine-client-3.0.7.0-Linux-x64.tar.gz')
             ->withPrefix($tdengine_prefix)
-            ->withFile('TDengine-ver-3.0.5.1.tar.gz')
             ->withBuildScript(
                 <<<EOF
                 mkdir -p build
@@ -23,15 +23,17 @@ return function (Preprocessor $p) {
                 LDFLAGS="$(pkg-config   --libs-only-L   --static \$PACKAGES )  " \
                 LIBS="$(pkg-config      --libs-only-l   --static \$PACKAGES )  "
                 cmake .. \
-                -DBUILD_TOOLS=false \
+                -DBUILD_TOOLS=OFF \
                 -DCMAKE_INSTALL_PREFIX={$tdengine_prefix} \
                 -DCMAKE_BUILD_TYPE=Release  \
                 -DCMAKE_POLICY_DEFAULT_CMP0074=NEW \
                 -DBUILD_STATIC_LIBS=ON \
                 -DBUILD_SHARED_LIBS=OFF  \
                 -DBUILD_TEST=OFF \
-                -DBUILD_HTTP=false \
-                -DJEMALLOC_ENABLED=true
+                -DBUILD_HTTP=OFF \
+                -DJEMALLOC_ENABLED=true \
+                -DBUILD_WITH_ICONV=ON \
+                -DBUILD_DOCS=OFF
 
 
               make -j \${LOGICAL_PROCESSORS}
