78a7a64e455a43489b2116175844efb1e7edcfe2
jingjingxyk <zonghengbaihe521@qq.com>
2023-07-28 06:59:24 +0800

    save changed

--- sapi/src/template/make.php
+++ sapi/src/template/make.php
@@ -188,14 +188,16 @@ export_variables() {
     CFLAGS=""
     LDFLAGS=""
     LIBS=""
-    LIBS=" $LIBS -Wl,--whole-archive -Wl,--start-group "
+    #LIBS=" $LIBS -Wl,--whole-archive -Wl,--start-group "
+    LIBS=" $LIBS  -Wl,--start-group "
 <?php foreach ($this->variables as $name => $value) : ?>
     <?= key($value) ?>="<?= current($value) ?>"
 <?php endforeach; ?>
 <?php foreach ($this->exportVariables as $value) : ?>
     export  <?= key($value) ?>="<?= current($value) ?>"
 <?php endforeach; ?>
-    export  LIBS=" $LIBS -Wl,--end-group -Wl,--no-whole-archive "
+    #export  LIBS=" $LIBS -Wl,--end-group -Wl,--no-whole-archive "
+    export  LIBS=" $LIBS -Wl,--end-group  "
 
     # export EXTRA_LIBS='<?= BROTLI_PREFIX ?>/lib/libbrotli.a <?= BROTLI_PREFIX ?>/lib/libbrotlicommon.a <?= BROTLI_PREFIX ?>/lib/libbrotlidec.a <?= BROTLI_PREFIX ?>/lib/libbrotlienc.a'
 
