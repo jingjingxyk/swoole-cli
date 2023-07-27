4331c7fe311249790b16ebe561a93b6bfe956469
jingjingxyk <zonghengbaihe521@qq.com>
2023-07-28 06:16:42 +0800

    save changed

--- sapi/src/template/make.php
+++ sapi/src/template/make.php
@@ -186,15 +186,15 @@ make_ext_hook() {
 export_variables() {
     CPPFLAGS=""
     CFLAGS=""
-    LDFLAGS=" -Wl,--start-group "
-    LIBS=" "
+    LDFLAGS="  "
+    LIBS=" -Wl,--start-group "
 <?php foreach ($this->variables as $name => $value) : ?>
     <?= key($value) ?>="<?= current($value) ?>"
 <?php endforeach; ?>
 <?php foreach ($this->exportVariables as $value) : ?>
     export  <?= key($value) ?>="<?= current($value) ?>"
 <?php endforeach; ?>
-    export  LDFLAGS = "$LIBS -Wl,--end-group "
+    export  LIBS="$LIBS -Wl,--end-group "
 
     # export EXTRA_LIBS='<?= BROTLI_PREFIX ?>/lib/libbrotli.a <?= BROTLI_PREFIX ?>/lib/libbrotlicommon.a <?= BROTLI_PREFIX ?>/lib/libbrotlidec.a <?= BROTLI_PREFIX ?>/lib/libbrotlienc.a'
 
