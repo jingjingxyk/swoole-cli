6776b6aea29db98ed7b9beeb5372ba2631493787
jingjingxyk <zonghengbaihe521@qq.com>
2023-07-28 07:19:18 +0800

    save changed

--- sapi/src/builder/library/brotli.php
+++ sapi/src/builder/library/brotli.php
@@ -47,9 +47,9 @@ EOF
             ->withPkgName('libbrotlidec')
             ->withBinPath($brotli_prefix . '/bin/')
     );
-    //$p->withVariable('CPPFLAGS', '$CPPFLAGS -I' . $brotli_prefix . '/include');
-    //$p->withVariable('LDFLAGS', '$LDFLAGS -L' . $brotli_prefix . '/lib');
-    //$p->withVariable('LIBS', '$LIBS -lbrotli  -lbrotlicommon -lbrotlienc  -lbrotlidec  ');
+    $p->withVariable('CPPFLAGS', '$CPPFLAGS -I' . $brotli_prefix . '/include');
+    $p->withVariable('LDFLAGS', '$LDFLAGS -L' . $brotli_prefix . '/lib');
+    $p->withVariable('LIBS', '$LIBS -lbrotli  -lbrotlicommon -lbrotlienc  -lbrotlidec  ');
 };
 // libbrotlicommon.a 应该优先被链接
 // 链接顺序问题
