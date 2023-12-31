b4fbb728e37371ce3cde355e8b1cea32fb72dd6d
jingjingxyk <zonghengbaihe521@qq.com>
2023-12-31 16:36:17 +0800

    update

--- sapi/src/builder/library/ffmpeg.php
+++ sapi/src/builder/library/ffmpeg.php
@@ -16,7 +16,7 @@ return function (Preprocessor $p) {
 
     $ldflags = $p->getOsType() == 'macos' ? ' ' : ' -static ';
     $cflags = $p->getOsType() == 'macos' ? ' ' : ' --static ';
-    $libs = $p->getOsType() == 'macos' ? ' -lc++ ' : ' -lstdc++ ';
+    $libs = $p->getOsType() == 'macos' ? ' -lc++ ' : ' -lstdc++ /usr/lib/libc.a /usr/lib/libstdc++.a ';
 
     $cppflags = $p->getOsType() == 'macos' ? ' ' : "  "; # -I/usr/include
     $ldfalgs = $p->getOsType() == 'macos' ? ' ' : "  "; #-L/usr/lib
