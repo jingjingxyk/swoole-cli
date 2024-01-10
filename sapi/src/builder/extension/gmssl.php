17c5b59518654fbe7221ef613d90da0f530311ab
jingjingxyk <zonghengbaihe521@qq.com>
2024-01-10 12:40:06 +0800

    验证 gmssl 库 构建

--- sapi/src/builder/extension/gmssl.php
+++ sapi/src/builder/extension/gmssl.php
@@ -8,6 +8,7 @@ return function (Preprocessor $p) {
         (new Extension('gmssl'))
             ->withOptions('--enable-gmssl')
             ->withUrl('https://github.com/GmSSL/GmSSL-PHP/archive/refs/tags/v1.1.0.tar.gz')
+            ->withFile('gmssl-v1.1.0.tar.gz')
             ->withHomePage('https://gmssl.github.io/GmSSL-PHP/')
             ->withManual('https://github.com/GmSSL/GmSSL-PHP#License-1-ov-file')
             ->withLicense('https://github.com/GmSSL/GmSSL-PHP/blob/main/LICENSE', Extension::LICENSE_PHP)
