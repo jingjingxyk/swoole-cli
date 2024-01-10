a411ee767ef6f4ce8c77652e947b7eea7bb254ba
jingjingxyk <zonghengbaihe521@qq.com>
2024-01-10 12:10:26 +0800

    add gmssl

---
+++ sapi/src/builder/extension/gmssl.php
@@ -0,0 +1,15 @@
+<?php
+
+use SwooleCli\Preprocessor;
+use SwooleCli\Extension;
+
+return function (Preprocessor $p) {
+    $p->addExtension(
+        (new Extension('gmssl'))
+            ->withOptions('--enable-gmssl')
+            ->withUrl('https://github.com/GmSSL/GmSSL-PHP/archive/refs/tags/v1.1.0.tar.gz')
+            ->withHomePage('https://gmssl.github.io/GmSSL-PHP/')
+            ->withManual('https://github.com/GmSSL/GmSSL-PHP#License-1-ov-file')
+            ->withLicense('https://github.com/GmSSL/GmSSL-PHP/blob/main/LICENSE', Extension::LICENSE_PHP)
+    );
+};
