8f346346d8b8bea220212b9256196037243b9e0b
jingjingxyk <zonghengbaihe521@qq.com>
2024-03-22 22:28:52 +0800

    默认启用swow扩展

--- sapi/src/Preprocessor.php
+++ sapi/src/Preprocessor.php
@@ -134,7 +134,7 @@ class Preprocessor
         'readline',
         'xml',
         'redis',
-        'swoole',
+        'swow',
         'yaml',
         'imagick',
         //'mongodb', //php8.2 需要处理依赖库问题 more info ： https://github.com/mongodb/mongo-php-driver/issues/1445
