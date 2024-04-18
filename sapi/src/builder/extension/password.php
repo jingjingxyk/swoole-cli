622f4c960d0d419be335e88d293795cbaf5f1b58
jingjingxyk <zonghengbaihe521@qq.com>
2024-04-18 19:23:42 +0800

    添加 password 验证

--- sapi/src/builder/extension/password.php
+++ sapi/src/builder/extension/password.php
@@ -10,8 +10,10 @@ return function (Preprocessor $p) {
             ->withHomePage('https://www.php.net/manual/en/password.installation.php')
             ->withLicense('https://github.com/php/php-src/blob/master/LICENSE', Extension::LICENSE_PHP)
             ->withManual('https://www.php.net/manual/en/refs.crypto.php')
-            ->withDependentLibraries('libsodium')
+            ->withDependentLibraries('libargon2')
     );
-    $p->withExportVariable('ARGON2_CFLAGS', '$(pkg-config  --cflags --static libsodium)');
-    $p->withExportVariable('ARGON2_LIBS', '$(pkg-config    --libs   --static libsodium)');
+    $p->withExportVariable('ARGON2_CFLAGS', '$(pkg-config  --cflags --static libargon2)');
+    $p->withExportVariable('ARGON2_LIBS', '$(pkg-config    --libs   --static libargon2)');
 };
+
+# libargon2 和 libsodium 互斥，出现函数多重定义
