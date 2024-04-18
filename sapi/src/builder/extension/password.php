03bc5b92a4df3caece240d34174ca1aaeb61c6e0
jingjingxyk <zonghengbaihe521@qq.com>
2024-04-18 19:14:49 +0800

    添加 password 验证

--- sapi/src/builder/extension/password.php
+++ sapi/src/builder/extension/password.php
@@ -4,10 +4,9 @@ use SwooleCli\Preprocessor;
 use SwooleCli\Extension;
 
 return function (Preprocessor $p) {
-    $libargon2_prefix = LIBARGON2_PREFIX;
     $p->addExtension(
         (new Extension('password'))
-            ->withOptions(' --with-password-argon2=' . $libargon2_prefix)
+            ->withOptions(' --with-password-argon2')
             ->withHomePage('https://www.php.net/manual/en/password.installation.php')
             ->withLicense('https://github.com/php/php-src/blob/master/LICENSE', Extension::LICENSE_PHP)
             ->withManual('https://www.php.net/manual/en/refs.crypto.php')
