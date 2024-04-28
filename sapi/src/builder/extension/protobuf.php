817f88ef278a3b9df00ed55b6aed55788d4b471e
jingjingxyk <zonghengbaihe521@qq.com>
2024-04-28 09:57:44 +0800

    merge code

--- sapi/src/builder/extension/protobuf.php
+++ sapi/src/builder/extension/protobuf.php
@@ -8,7 +8,7 @@ return function (Preprocessor $p) {
         (new Extension('protobuf'))
             ->withOptions('--enable-protobuf')
             ->withPeclVersion('3.21.6')
-            ->withHash('md5','30fd6011881fa67878805c394e425577')
+            ->withFileHash('md5','30fd6011881fa67878805c394e425577')
             ->withHomePage('https://developers.google.com/protocol-buffers')
             ->withManual('https://protobuf.dev/reference/php/php-generated/')
     );
