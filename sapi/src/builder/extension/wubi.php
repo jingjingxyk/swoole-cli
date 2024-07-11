ee9fc294a2b57bfddfb8850fbf3e0ec0eac7c90c
jingjingxyk <zonghengbaihe521@qq.com>
2024-07-11 22:20:19 +0800

    update

--- sapi/src/builder/extension/wubi.php
+++ sapi/src/builder/extension/wubi.php
@@ -5,13 +5,13 @@ use SwooleCli\Preprocessor;
 use SwooleCli\Extension;
 
 return function (Preprocessor $p) {
+    $ext_dir = $p->getWorkDir();
     $options = ' --enable-wubi  ';
     $ext = (new Extension('wubi'))
         ->withOptions($options)
         ->withLicense('https://www.php.net/license/3_01.txt', Extension::LICENSE_PHP)
         ->withHomePage('https://github.com/jingjingxyk/wubi-src')
-        ->withManual('https://wubii.jingjingxyk.com/#/')
-        /*
+        ->withManual('https://wubi.jingjingxyk.com/#/')
         ->withFile('wubi-latest.tar.gz')
         ->withAutoUpdateFile()
         ->withDownloadScript(
@@ -23,7 +23,6 @@ return function (Preprocessor $p) {
 EOF
         )
         ->withBuildCached(false);
-        */
     ;
     $p->addExtension($ext);
 
