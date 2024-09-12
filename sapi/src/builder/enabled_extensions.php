c90709c8a8e43cb52b975f8a00f0a74a7271bae6
jingjingxyk <zonghengbaihe521@qq.com>
2024-09-13 00:19:43 +0800

    update

--- sapi/src/builder/enabled_extensions.php
+++ sapi/src/builder/enabled_extensions.php
@@ -32,7 +32,7 @@ return [
     'xml',
     'gd',
     'redis',
-    'swoole',
+    //'swoole', //php 8.0 静态编译 swoole 报错
     'yaml',
     'imagick',
     //'mongodb',//php8.2 需要处理依赖库问题 more info ： https://github.com/mongodb/mongo-php-driver/issues/1445
