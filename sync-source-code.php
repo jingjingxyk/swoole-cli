4f645d0b444bf87a4242e963216c5e12710689c4
jingjingxyk <zonghengbaihe521@qq.com>
2024-10-06 16:14:40 +0800

    test

--- sync-source-code.php
+++ sync-source-code.php
@@ -152,14 +152,13 @@ PHP_OPCACHE_H_EOF
     # 在sed命令中，常见的需要转义的字符有：\、/、$、&、.、*、[、]等
     #                                反斜杠、正斜杠、美元符号、引用符号、点号、星号、方括号等
 
-    test -f main/main.c.backup && rm -f main/main.c.backup
-    test -f ext/opcache/config.m4.backup && rm -f ext/opcache/config.m4.backup
+
 
 
     # fpm
     # cp -rf $SRC/sapi/fpm/fpm ./sapi/cli/
     sed -i.backup 's/int main(int argc, char \*argv\[\])/int fpm_main(int argc, char \*argv\[\])/g' ./sapi/cli/fpm/fpm_main.c
-    sed -i.backup "s/{'-', 0, NULL}/{'P', 0, \"fpm\"},\n	{'-', 0, NULL}/g" ./sapi/cli/fpm/fpm_main.c
+    # sed -i.backup "s/{'-', 0, NULL}/{'P', 0, \"fpm\"},\n	{'-', 0, NULL}/g" ./sapi/cli/fpm/fpm_main.c
 
 
 
@@ -167,6 +166,11 @@ PHP_OPCACHE_H_EOF
     # cp -rf $SRC/sapi/cli/ps_title.c ./sapi/cli
     cp -rf $SRC/sapi/cli/generate_mime_type_map.php ./sapi/cli
     cp -rf $SRC/sapi/cli/php.1.in ./sapi/cli
+
+    # clean file
+    test -f main/main.c.backup && rm -f main/main.c.backup
+    test -f ext/opcache/config.m4.backup && rm -f ext/opcache/config.m4.backup
+    test -f sapi/cli/fpm/fpm_main.c.backup && rm -f sapi/cli/fpm/fpm_main.c.backup
 
 EOF;
 
