acfd2cd118ab594227bedfe7e480e385f76b513c
jingjingxyk <zonghengbaihe521@qq.com>
2024-10-06 15:39:34 +0800

    test

--- sync-source-code.php
+++ sync-source-code.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 $project_dir = __DIR__;
 $php_source_folder = require_once __DIR__ . '/sapi/scripts/download-php-src-archive.php';
 $sync_dest_dir = $project_dir . '/var/sync-source-code-tmp';
-
 $scanned_directory_source = array_diff(scandir($php_source_folder . '/ext/'), array('..', '.'));
 $scanned_directory_destination = array_diff(scandir($project_dir . '/ext/'), array('..', '.'));
 
