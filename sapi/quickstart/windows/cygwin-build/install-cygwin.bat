6e37986a794920a17117ea193c7a38a6b8fb6018
jingjingxyk <zonghengbaihe521@qq.com>
2025-01-01 22:41:08 +0800

    update

--- sapi/quickstart/windows/cygwin-build/install-cygwin.bat
+++ sapi/quickstart/windows/cygwin-build/install-cygwin.bat
@@ -33,7 +33,7 @@ if "%GITHUB_ACTIONS%"=="" (
 )
 
 setup-x86_64.exe  --no-desktop --no-shortcuts --no-startmenu --quiet-mode --disable-buggy-antivirus  --site  %SITE% --packages make,git,curl,wget,tar,libtool,bison,gcc-g++,autoconf,automake,openssl,libpcre2-devel,libssl-devel,libcurl-devel,libxml2-devel,libxslt-devel,libgmp-devel,ImageMagick,libpng-devel,libjpeg-devel,libfreetype-devel,libwebp-devel,libsqlite3-devel,zlib-devel,libbz2-devel,liblz4-devel,liblzma-devel,libzip-devel,libicu-devel,libonig-devel,libcares-devel,libsodium-devel,libyaml-devel,libMagick-devel,libzstd-devel,libbrotli-devel,libreadline-devel,libintl-devel,libpq-devel,libssh2-devel,libidn2-devel,gettext-devel,coreutils
-setup-x86_64.exe  --no-desktop --no-shortcuts --no-startmenu --quiet-mode --disable-buggy-antivirus  --site  %SITE% --packages setup-x86_64.exe --quiet-mode --disable-buggy-antivirus --site $SITE --packages zip unzip icu libicu-devel
-setup-x86_64.exe  --no-desktop --no-shortcuts --no-startmenu --quiet-mode --disable-buggy-antivirus  --site  %SITE% --packages setup-x86_64.exe --quiet-mode --disable-buggy-antivirus --site $SITE --packages libwrap-devel
+setup-x86_64.exe  --no-desktop --no-shortcuts --no-startmenu --quiet-mode --disable-buggy-antivirus  --site  %SITE% --packages zip unzip icu libicu-devel
+setup-x86_64.exe  --no-desktop --no-shortcuts --no-startmenu --quiet-mode --disable-buggy-antivirus  --site  %SITE% --packages libwrap-devel
 
 endlocal
