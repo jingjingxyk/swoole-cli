963d03d35275cfd529d118599459a0f3191e7c97
jingjingxyk <zonghengbaihe521@qq.com>
2025-02-28 21:41:18 +0800

    enable cygwin openssh

--- sapi/scripts/cygwin/cygwin-build.sh
+++ sapi/scripts/cygwin/cygwin-build.sh
@@ -13,28 +13,6 @@ cd ${__PROJECT__}
 mkdir -p bin
 
 WORK_TEMP_DIR=${__PROJECT__}/var/cygwin-build/
-cd ${WORK_TEMP_DIR}/php-src/
+cd ${WORK_TEMP_DIR}/openssh/
 
-mkdir -p bin/.libs
-# export LDFLAGS="-all-static"
-
-LOGICAL_PROCESSORS=$(nproc)
-set +u
-if [ -n "${GITHUB_ACTION}" ]; then
-  if test $LOGICAL_PROCESSORS -ge 4; then
-    LOGICAL_PROCESSORS=$((LOGICAL_PROCESSORS - 2))
-  fi
-  make cli
-  # make -j $LOGICAL_PROCESSORS
-else
-  make -j $LOGICAL_PROCESSORS cli
-fi
-set -u
-
-${WORK_TEMP_DIR}/php-src/sapi/cli/php.exe -v
-
-cp -f ${WORK_TEMP_DIR}/php-src/sapi/cli/php.exe ${__PROJECT__}/bin/
-
-${__PROJECT__}/bin/php.exe -v
-${__PROJECT__}/bin/php.exe -m
-${__PROJECT__}/bin/php.exe --ri swoole
+make -j $(nproc)
