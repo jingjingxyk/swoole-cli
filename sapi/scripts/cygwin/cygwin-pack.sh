a16da4146c8abfa1e6ae06239575c1ee5144e2da
jingjingxyk <zonghengbaihe521@qq.com>
2025-03-01 16:34:49 +0800

    update cygwin build privoxy

--- sapi/scripts/cygwin/cygwin-pack.sh
+++ sapi/scripts/cygwin/cygwin-pack.sh
@@ -18,15 +18,15 @@ APP_VERSION=$(${WORK_TEMP_DIR}/privoxy/privoxy.exe --help | grep 'Privoxy versio
 NAME="privoxy-${APP_VERSION}-cygwin-x64"
 
 test -d /tmp/${NAME} && rm -rf /tmp/${NAME}
-mkdir -p /tmp/${NAME}/bin/
+mkdir -p /tmp/${NAME}/sbin/
 
 cd ${__PROJECT__}/
 ldd ${WORK_TEMP_DIR}/privoxy/privoxy.exe | grep -v '/cygdrive/' | awk '{print $3}'
-ldd ${WORK_TEMP_DIR}/privoxy/privoxy.exe | grep -v '/cygdrive/' | awk '{print $3}' | xargs -I {} cp {} /tmp/${NAME}/bin/
+ldd ${WORK_TEMP_DIR}/privoxy/privoxy.exe | grep -v '/cygdrive/' | awk '{print $3}' | xargs -I {} cp {} /tmp/${NAME}/sbin/
 
 ls -lh /tmp/${NAME}/
 
-cp -f ${WORK_TEMP_DIR}/privoxy/privoxy.exe /tmp/${NAME}/bin/
+cp -rf /usr/local/swoole-cli/privoxy/* /tmp/${NAME}/
 
 cd /tmp/${NAME}/
 
