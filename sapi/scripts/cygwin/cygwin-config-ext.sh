cc7a9a4230b0ba1a3caf5e2194d71ae6f43b5c16
jingjingxyk <zonghengbaihe521@qq.com>
2025-03-02 22:29:13 +0800

    update

--- sapi/scripts/cygwin/cygwin-config-ext.sh
+++ sapi/scripts/cygwin/cygwin-config-ext.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -x
+set -eux
 __DIR__=$(
   cd "$(dirname "$0")"
   pwd
@@ -34,16 +34,17 @@ mkdir -p ${WORK_TEMP_DIR}/openssh
 cd ${__PROJECT__}/pool/lib
 if [ ! -f openssh-${OPENSSH_VERSION}.tgz ]; then
   cd ${__PROJECT__}/var/
-  test -d openssh && rm -rf openssh
+  if [[ -d openssh ]] ; then
+    rm -rf openssh
+  fi
   git clone -b ${OPENSSH_VERSION} https://anongit.mindrot.org/openssh.git
   cd openssh
   tar -czvf ${__PROJECT__}/pool/lib/openssh-${OPENSSH_VERSION}.tgz .
-
   cd ${__PROJECT__}/
-
 fi
 
 cd ${__PROJECT__}/
+
 tar --strip-components=1 -C ${WORK_TEMP_DIR}/openssh -xf ${__PROJECT__}/pool/lib/openssh-${OPENSSH_VERSION}.tgz
 
 cd ${__PROJECT__}
