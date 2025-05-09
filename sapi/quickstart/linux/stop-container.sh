a3e4d97cdf2e431299e9c817d94f7212c25a570a
jingjingxyk <zonghengbaihe521@qq.com>
2025-05-09 17:35:56 +0800

    update

--- sapi/quickstart/linux/stop-container.sh
+++ sapi/quickstart/linux/stop-container.sh
@@ -9,10 +9,20 @@ cd ${__DIR__}
 
 {
   docker stop swoole-cli-alpine-dev
+  docker rm swoole-cli-alpine-dev
+
   docker stop swoole-cli-debian-dev
+  docker rm swoole-cli-debian-dev
+
   docker stop swoole-cli-rhel-dev
+  docker rm swoole-cli-rhel-dev
+
   docker stop swoole-cli-ubuntu-dev
+  docker rm swoole-cli-ubuntu-dev
+
   docker stop swoole-cli-builder
+  docker rm swoole-cli-builder
+
   sleep 5
 } || {
   echo $?
