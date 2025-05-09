e31dda1b0e2c4db3b3786c40ee59e62a40eefce5
jingjingxyk <zonghengbaihe521@qq.com>
2025-05-09 22:52:06 +0800

    update

--- sapi/src/builder/library/ovs.php
+++ sapi/src/builder/library/ovs.php
@@ -5,6 +5,12 @@ use SwooleCli\Preprocessor;
 
 return function (Preprocessor $p) {
     $ovs_prefix = OVS_PREFIX;
+    $cflags = '';
+    $ldflags = '';
+    if ($p->isLinux()) {
+        $cflags .= ' -static -fPIE ';
+        $ldflags .= ' -static  '; //-static-pie
+    }
     $lib = new Library('ovs');
     $lib->withHomePage('https://github.com/openvswitch/ovs/')
         ->withLicense('https://github.com/openvswitch/ovs/blob/master/LICENSE', Library::LICENSE_APACHE2)
@@ -27,7 +33,7 @@ EOF
         ./configure --help
         PACKAGES="openssl"
         CPPFLAGS="$(pkg-config  --cflags-only-I --static \$PACKAGES ) " \
-        LDFLAGS="$(pkg-config   --libs-only-L   --static \$PACKAGES ) " \
+        LDFLAGS="$(pkg-config   --libs-only-L   --static \$PACKAGES ) {$ldflags}" \
         LIBS="$(pkg-config      --libs-only-l   --static \$PACKAGES ) " \
         ./configure \
         --prefix={$ovs_prefix} \
@@ -40,11 +46,10 @@ EOF
 
 EOF
         )
-        //->withMakeOptions( " dist-docs ")
-        //->withPkgName('libofproto')
-        //->withPkgName('libopenvswitch')
-        //->withPkgName('libovsdb')
-        //->withPkgName('libsflow')
+        ->withPkgName('libofproto')
+        ->withPkgName('libopenvswitch')
+        ->withPkgName('libovsdb')
+        ->withPkgName('libsflow')
         ->withBinPath($ovs_prefix . '/bin/')
         ->withDependentLibraries('openssl') //'dpdk','unbound', 'libcap_ng'
     ;
