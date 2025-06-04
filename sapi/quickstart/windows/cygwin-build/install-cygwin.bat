ba572c9cc6337f4320d47014936a6407484600e6
jingjingxyk <zonghengbaihe521@qq.com>
2025-06-04 16:30:54 +0800

    test

--- sapi/quickstart/windows/cygwin-build/install-cygwin.bat
+++ sapi/quickstart/windows/cygwin-build/install-cygwin.bat
@@ -35,11 +35,13 @@ if defined GITHUB_ACTIONS (
 )
 
 :: package  separate with commas
-set "PACKAGES=make,git,curl,wget,tar,libtool,bison,gcc-g++,autoconf,automake,openssl,libpcre2-devel,libssl-devel,libcurl-devel,libxml2-devel,libxslt-devel,libgmp-devel,ImageMagick,libpng-devel,libjpeg-devel,libfreetype-devel,libwebp-devel,libsqlite3-devel,zlib-devel,libbz2-devel,liblz4-devel,liblzma-devel,libzip-devel,libicu-devel,libonig-devel,libcares-devel,libsodium-devel,libyaml-devel,libMagick-devel,libzstd-devel,libbrotli-devel,libreadline-devel,libintl-devel,libpq-devel,libssh2-devel,libidn2-devel,gettext-devel,coreutils"
+set "PACKAGES=make,git,curl,wget,tar,libtool,bison,gcc-g++,autoconf,automake,openssl,libpcre2-devel,libssl-devel,libcurl-devel,libxml2-devel,libxslt-devel,libgmp-devel,ImageMagick,libpng-devel,libjpeg-devel,libfreetype-devel,libwebp-devel,libsqlite3-devel,zlib-devel,libbz2-devel,liblz4-devel,liblzma-devel,libzip-devel,libonig-devel,libcares-devel,libsodium-devel,libyaml-devel,libMagick-devel,libzstd-devel,libbrotli-devel,libreadline-devel,libintl-devel,libpq-devel,libssh2-devel,libidn2-devel,gettext-devel,coreutils"
 set "PACKAGES=%PACKAGES%,zip,unzip"
 set "PACKAGES=%PACKAGES%,libpq5,libpq-devel"
 set "PACKAGES=%PACKAGES%,libzstd-devel"
 set "PACKAGES=%PACKAGES%,cygwin-devel,libnet6-devel"
+rem set "PACKAGES=%PACKAGES%,libicu-devel"
+set "PACKAGES=%PACKAGES%,libicu-devel-74.2-1,libicu74-74.2-1"
 
 set "OPTIONS=%OPTIONS% --packages %PACKAGES%"
 echo %OPTIONS%
