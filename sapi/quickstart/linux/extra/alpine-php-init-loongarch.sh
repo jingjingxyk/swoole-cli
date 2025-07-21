26143abf325cdfc6f2aefa2aca79bd7e136d310c
jingjingxyk <zonghengbaihe521@qq.com>
2025-07-21 21:48:07 +0800

    update

--- sapi/quickstart/linux/extra/alpine-php-init-loongarch.sh
+++ sapi/quickstart/linux/extra/alpine-php-init-loongarch.sh
@@ -3,7 +3,6 @@ apk add php7-iconv php7-mbstring php7-phar php7-openssl
 apk add php7-posix php7-tokenizer php7-intl
 apk add php7-dom php7-xmlwriter php7-xml php7-simplexml
 apk add php7-pdo php7-sockets php7-curl php7-mysqlnd php7-pgsql php7-sqlite3
-apk add php7-redis php7-mongodb
 apk add php7-redis php7-fileinfo
 
 php7 -v
