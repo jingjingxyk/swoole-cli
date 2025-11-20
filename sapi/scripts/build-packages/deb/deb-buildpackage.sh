b0742ffb84eac53383503eb12086ff9e6ecbd8c8
jingjingxyk <zonghengbaihe521@qq.com>
2025-11-20 22:31:26 +0800

    update

--- sapi/scripts/build-packages/deb/deb-buildpackage.sh
+++ sapi/scripts/build-packages/deb/deb-buildpackage.sh
@@ -11,6 +11,19 @@ __PROJECT__=$(
 )
 
 cd ${__DIR__}
-# cd ${__PROJECT__}
+
 
 dpkg -b swoole-cli ${__PROJECT__}/swoole-cli_v6.1.1.0_amd64.deb
+
+
+cd ${__PROJECT__}
+# dpkg -i swoole-cli_v6.1.1.0_amd64.deb
+
+dpkg-deb -c swoole-cli_v6.1.1.0_amd64.deb # 查看包内文件列表
+dpkg-deb -I swoole-cli_v6.1.1.0_amd64.deb # 查看包元信息
+
+exit 0
+# 安装测试‌
+sudo dpkg -i swoole-cli_v6.1.1.0_amd64.deb # 安装包
+dpkg -l swoole-cli # 验证安装
+dpkg -L swoole-cli # 查看安装的文件位置
