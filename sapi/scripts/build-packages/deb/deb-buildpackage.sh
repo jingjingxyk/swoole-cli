6230276ce13c3d835dbf51598f65a6ad09c86572
jingjingxyk <zonghengbaihe521@qq.com>
2025-11-23 20:47:36 +0800

    update

--- sapi/scripts/build-packages/deb/deb-buildpackage.sh
+++ sapi/scripts/build-packages/deb/deb-buildpackage.sh
@@ -12,35 +12,34 @@ __PROJECT__=$(
 
 cd ${__DIR__}
 
-
 sudo apt install -y \
-libssl-dev \
-libpq-dev libpq5 \
-libcurl4-openssl-dev \
-libicu70 libbz2-dev \
-libzstd-dev libdeflate-dev libzip-dev \
-libsqlite3-dev libgmp-dev libexif-dev \
-libxmltok1-dev libsodium-dev libfreetype-dev libjpeg-dev libwebp-dev libavif-dev \
-libaom-dev libspng-dev libcares2 libbrotli-dev libwoff-dev \
-libarchive-dev libonig-dev \
-re2c \
-libreadline-dev \
-libxslt1-dev \
-libyaml-dev
-
-
+  libssl-dev \
+  libpq-dev libpq5 \
+  libcurl4-openssl-dev \
+  libicu70 libbz2-dev \
+  libzstd-dev libdeflate-dev libzip-dev \
+  libsqlite3-dev libgmp-dev libexif-dev \
+  libxmltok1-dev libsodium-dev libfreetype-dev libjpeg-dev libwebp-dev libavif-dev \
+  libaom-dev libspng-dev libcares2 libbrotli-dev libwoff-dev \
+  libarchive-dev libonig-dev \
+  re2c \
+  libreadline-dev \
+  libxslt1-dev \
+  libyaml-dev
 
 bash ${__PROJECT__}/build-release-example.sh
-
+cd ${__PROJECT__}
+mkdir -p ext/swoole
+git clone -b v6.1.2 https://github.com/swoole/swoole-src.git ext/swoole/
+cd ${__PROJECT__}
 bash ${__PROJECT__}/sapi/scripts/build-swoole-cli-with-linux-gcc.sh
 
 chmod 755 swoole-cli/usr/local/bin/swoole-cli
 
 # shellcheck disable=SC2067
-find swoole-cli -type d -exec chmod 755 {} ;
+find swoole-cli -type d -exec chmod 755 {}
 
 dpkg -b swoole-cli ${__PROJECT__}/swoole-cli_v6.1.1.0_amd64.deb
-
 
 cd ${__PROJECT__}
 # dpkg -i swoole-cli_v6.1.1.0_amd64.deb
@@ -51,5 +50,5 @@ dpkg-deb -I swoole-cli_v6.1.1.0_amd64.deb # 查看包元信息
 exit 0
 # 安装测试‌
 sudo dpkg -i swoole-cli_v6.1.1.0_amd64.deb # 安装包
-dpkg -l swoole-cli # 验证安装
-dpkg -L swoole-cli # 查看安装的文件位置
+dpkg -l swoole-cli                         # 验证安装
+dpkg -L swoole-cli                         # 查看安装的文件位置
