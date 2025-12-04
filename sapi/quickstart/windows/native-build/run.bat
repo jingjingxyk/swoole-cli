a18e6a229f3866ecc9fc0868fc95e3b2281e1a79
jingjingxyk <zonghengbaihe521@qq.com>
2025-12-04 17:10:20 +0800

    update

--- sapi/quickstart/windows/native-build/run.bat
+++ sapi/quickstart/windows/native-build/run.bat
@@ -10,6 +10,7 @@ set "__PROJECT__=%cd%"
 echo %cd%
 
 rem var\windows-build-deps\php-sdk-binary-tools\phpsdk-starter.bat -c vs17 -a x64  -t sapi\quickstart\windows\native-build\run.bat
+rem D:\a\swoole-cli\swoole-cli\var\windows-build-deps\php-src\x64\Release_TS
 
 call %__PROJECT__%\sapi\quickstart\windows\native-build\config.bat
 call %__PROJECT__%\sapi\quickstart\windows\native-build\x_custom_config.bat
