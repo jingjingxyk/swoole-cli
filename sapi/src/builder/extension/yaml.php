979dcfaf13237fe125dd4dc7d28c0f9550030d38
jingjingxyk <zonghengbaihe521@qq.com>
2026-01-03 19:55:15 +0800

    update mongodb

--- sapi/src/builder/extension/yaml.php
+++ sapi/src/builder/extension/yaml.php
@@ -8,8 +8,7 @@ return function (Preprocessor $p) {
     $p->addExtension(
         (new Extension('yaml'))
             ->withOptions('--with-yaml=' . LIBYAML_PREFIX)
-            ->withPeclVersion('2.2.2')
-            ->withFileHash('md5', '22678c1238f2c6848ff7d74b780a8307')
+            ->withPeclVersion('2.3.0')
             ->withHomePage('https://github.com/php/pecl-file_formats-yaml')
             ->withLicense('https://github.com/php/pecl-file_formats-yaml/blob/php7/LICENSE', Extension::LICENSE_MIT)
             ->withDependentLibraries('libyaml')
