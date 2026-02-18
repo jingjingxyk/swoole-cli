d3f747a875e883c01f9342ea61d56198cab69923
jingjingxyk <zonghengbaihe521@qq.com>
2026-02-18 21:29:51 +0800

    update

--- sapi/quickstart/linux/extra/alpine-php-init.sh
+++ sapi/quickstart/linux/extra/alpine-php-init.sh
@@ -1,16 +1,16 @@
 apk search php
-apk add php82-cli php82-dev
-apk add php82-iconv php82-mbstring php82-phar php82-openssl
-apk add php82-posix php82-tokenizer php82-intl
-apk add php82-dom php82-xmlwriter php82-xml php82-simplexml
-apk add php82-pdo php82-sockets php82-curl php82-mysqlnd php82-pgsql php82-sqlite3
-apk add php82-redis php82-mongodb
-apk add php82-redis php82-fileinfo
+apk add php84-cli php84-dev
+apk add php84-iconv php84-mbstring php84-phar php84-openssl
+apk add php84-posix php84-tokenizer php84-intl
+apk add php84-dom php84-xmlwriter php84-xml php84-simplexml
+apk add php84-pdo php84-sockets php84-curl php84-mysqlnd php84-pgsql php84-sqlite3
+apk add php84-redis php84-mongodb
+apk add php84-redis php84-fileinfo
 
-php82 -v
-php82 --ini
-php82 --ini | grep ".ini files"
+php84 -v
+php84 --ini
+php84 --ini | grep ".ini files"
 
-ln -sf /usr/bin/php82 /usr/bin/php
+ln -sf /usr/bin/php84 /usr/bin/php
 ln -sf /usr/bin/phpize82 /usr/bin/phpize
 ln -sf /usr/bin/php-config82 /usr/bin/php-config
