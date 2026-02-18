2c331291d42ca3ee8440284d91484683fec77f8d
jingjingxyk <zonghengbaihe521@qq.com>
2026-02-18 19:39:12 +0800

    update extension redis yaml imagick file hash

--- sapi/src/builder/extension/imagick.php
+++ sapi/src/builder/extension/imagick.php
@@ -9,7 +9,7 @@ return function (Preprocessor $p) {
         (new Extension('imagick'))
             ->withOptions('--with-imagick=' . IMAGEMAGICK_PREFIX)
             ->withPeclVersion('3.8.1')
-            //->withFileHash('md5', 'e6185b1412e65a91e598d1c79a00aeb9')
+            ->withFileHash('sha256', '3a3587c0a524c17d0dad9673a160b90cd776e836838474e173b549ed864352ee')
             ->withHomePage('https://github.com/Imagick/imagick')
             ->withLicense('https://github.com/Imagick/imagick/blob/master/LICENSE', Extension::LICENSE_PHP)
             ->withDependentLibraries('imagemagick')
--- sapi/src/builder/extension/yaml.php
+++ sapi/src/builder/extension/yaml.php
@@ -9,6 +9,7 @@ return function (Preprocessor $p) {
         (new Extension('yaml'))
             ->withOptions('--with-yaml=' . LIBYAML_PREFIX)
             ->withPeclVersion('2.3.0')
+            ->withFileHash('sha256', 'bc8404807a3a4dc896b310af21a7f8063aa238424ff77f27eb6ffa88b5874b8a')
             ->withHomePage('https://github.com/php/pecl-file_formats-yaml')
             ->withLicense('https://github.com/php/pecl-file_formats-yaml/blob/php7/LICENSE', Extension::LICENSE_MIT)
             ->withDependentLibraries('libyaml')
