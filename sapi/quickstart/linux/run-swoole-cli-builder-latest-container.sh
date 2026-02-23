06bce7c6d47c4805ed701af051115d02aaec75b6
jingjingxyk <zonghengbaihe521@qq.com>
2026-02-23 21:10:31 +0800

    update

--- sapi/quickstart/linux/run-swoole-cli-builder-latest-container.sh
+++ sapi/quickstart/linux/run-swoole-cli-builder-latest-container.sh
@@ -19,7 +19,7 @@ cd ${__DIR__}
 }
 cd ${__DIR__}
 
-IMAGE=alpine:3.18
+IMAGE=swoole-cli-builder:latest
 
 cd ${__DIR__}
 docker run --rm --name swoole-cli-builder-latest -d -v ${__PROJECT__}:/work -w /work --init $IMAGE tail -f /dev/null
