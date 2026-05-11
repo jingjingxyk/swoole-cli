085290b08042499b0b6b040faf23fab6e455e28d
jingjingxyk <zonghengbaihe521@qq.com>
2026-05-11 12:44:52 +0800

    test pgsql

--- sapi/src/builder/library/pgsql.php
+++ sapi/src/builder/library/pgsql.php
@@ -18,11 +18,61 @@ return function (Preprocessor $p) {
         (new Library('pgsql'))
             ->withHomePage('https://www.postgresql.org/')
             ->withLicense('https://www.postgresql.org/about/licence/', Library::LICENSE_SPEC)
-            ->withUrl('https://ftp.postgresql.org/pub/source/v16.3/postgresql-16.3.tar.gz')
-            ->withManual('https://www.postgresql.org/docs/current/install-procedure.html#CONFIGURE-OPTIONS')
+            //->withUrl('https://ftp.postgresql.org/pub/source/v16.3/postgresql-16.3.tar.gz')
+            ->withUrl('https://ftp.postgresql.org/pub/source/v18.3/postgresql-18.3.tar.gz')
             ->withManual('https://www.postgresql.org/docs/current/install-procedure.html#CONFIGURE-OPTIONS#:~:text=Client-only%20installation')
-            ->withFileHash('md5', '8a58db4009e1a50106c5e1a8c4b03bed')
+            //->withFileHash('md5', '8a58db4009e1a50106c5e1a8c4b03bed')
             ->withPrefix($pgsql_prefix)
+            ->withBuildScript(
+                <<<EOF
+            {$custom_env_start}
+            test -d build && rm -rf build
+            mkdir -p build
+            cd build
+
+            ../configure --help
+
+            sed -i.backup "s/invokes exit\'; exit 1;/invokes exit\';/"  ../src/interfaces/libpq/Makefile
+
+            sed -i.backup "278 s/^/# /"  ../src/Makefile.shlib
+            sed -i.backup "402 s/^/# /"  ../src/Makefile.shlib
+
+            PACKAGES="libssl libcrypto openssl zlib icu-uc icu-io icu-i18n readline libxml-2.0  libxslt libzstd liblz4"
+            CPPFLAGS="$(pkg-config  --cflags-only-I --static \$PACKAGES )" \
+            LDFLAGS="$(pkg-config   --libs-only-L   --static \$PACKAGES ) {$ldflags} " \
+            LIBS="$(pkg-config      --libs-only-l   --static \$PACKAGES ) {$libs}  " \
+            ../configure  \
+            --prefix={$pgsql_prefix} \
+            --enable-coverage=no \
+            --with-openssl \
+            --with-ssl=openssl  \
+            --with-readline \
+            --with-icu \
+            --with-libxml  \
+            --with-libxslt \
+            --with-lz4 \
+            --with-zstd \
+            --without-ldap \
+            --without-perl \
+            --without-python \
+            --without-pam \
+            --without-ldap \
+            --without-bonjour \
+            --without-tcl
+
+            make -C src/bin/pg_config install
+
+            make -C src/include install
+
+            make -C  src/common install
+
+            make -C  src/port install
+
+            make -C  src/interfaces/libpq install
+
+            {$custom_env_end}
+EOF
+            )
             ->withBuildScript(
                 <<<EOF
             {$custom_env_start}
