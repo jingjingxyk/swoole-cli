63b9eaede978b85a88f6fb3c8c3fdaadb532e459
jingjingxyk <zonghengbaihe521@qq.com>
2026-06-23 18:34:14 +0800

    update

--- sapi/scripts/windows-native/build.bat
+++ sapi/scripts/windows-native/build.bat
@@ -26,8 +26,8 @@ nmake /E /f Makefile  x-release-static-php
 
 
 
-:: .\x64\Release\php.exe -v
-:: .\x64\Release\php.exe -m
+.\x64\Release\php.exe -v
+.\x64\Release\php.exe -m
 :: dumpbin /DEPENDENTS ".\x64\Release\php.exe"
 dumpbin /DEPENDENTS ".\x64\Release_TS\php.exe"
 
