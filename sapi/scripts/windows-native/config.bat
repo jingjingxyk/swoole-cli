38e8af8844dabf213543c7db35ed78c030ceb01b
jingjingxyk <zonghengbaihe521@qq.com>
2026-06-23 11:21:24 +0800

    update

--- sapi/scripts/windows-native/config.bat
+++ sapi/scripts/windows-native/config.bat
@@ -40,7 +40,7 @@ set "CFLAGS=/EHsc /MP /MT /UCRT  "
 
 rem https://learn.microsoft.com/zh-cn/cpp/c-runtime-library/crt-library-features?view=msvc-170
 
-set "LDFLAGS=/MT /VERBOSE:LIB 		"
+set "LDFLAGS=/VERBOSE:LIB 		"
 ::set "LDFLAGS=/VERBOSE:LIB 	/NODEFAULTLIB:msvcrt.lib /NODEFAULTLIB:msvcrtd.lib /NODEFAULTLIB:libcmtd.lib /DEFAULTLIB:libcmt.lib  /DEFAULTLIB:libucrt.lib /DEFAULTLIB:libcpmt.lib /DEFAULTLIB:libvcruntime.lib	/NODEFAULTLIB:libucrtd.lib  /NODEFAULTLIB:ucrt.lib /NODEFAULTLIB:ucrtd.lib	"
 
 rem set "LDFLAGS=/WHOLEARCHIVE /FORCE:MULTIPLE"
