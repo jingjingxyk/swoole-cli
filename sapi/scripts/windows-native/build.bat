17c7b66e3109794fedbdf1d5fc9c9a43f50fbb24
jingjingxyk <zonghengbaihe521@qq.com>
2026-06-24 18:19:15 +0800

    update

--- sapi/scripts/windows-native/build.bat
+++ sapi/scripts/windows-native/build.bat
@@ -21,9 +21,23 @@ rem nmake   mode=static debug=false
 
 :: nmake /E php.exe
 :: exit /b
-nmake /E /f Makefile  x-show-var
-nmake /E /f Makefile  x-release-static-php mode=static
+set "LIBS_CLI=kernel32.lib ole32.lib user32.lib advapi32.lib shell32.lib ws2_32.lib Dnsapi.lib psapi.lib bcrypt.lib"
+set "LIBS_CLI=%LIBS_CLI% zlib"
 
+nmake /E /f Makefile  x-show-var
+nmake /E /n /f Makefile  x-release-static-php mode=static LIBS_CLI="%LIBS_CLI%"
+
+
+
+IPHLPAPI.DLL
+    zlib.dll
+    KERNEL32.dll
+    ole32.dll
+    ADVAPI32.dll
+    SHELL32.dll
+    WS2_32.dll
+    DNSAPI.dll
+    bcrypt.dll
 
 
 .\x64\Release_TS\php.exe -v
