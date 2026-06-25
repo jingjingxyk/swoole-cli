79a61cd48c55d882afb75f1ff0e2056f6a2262ce
jingjingxyk <zonghengbaihe521@qq.com>
2026-06-25 08:44:33 +0800

    update

--- sapi/scripts/windows-native/build.bat
+++ sapi/scripts/windows-native/build.bat
@@ -26,10 +26,14 @@ set "LIBS_CLI=%LIBS_CLI% zlibstatic.lib"
 rem LIBS_CLI="%LIBS_CLI%"
 
 nmake /E /f Makefile  x-show-var
-nmake /E /f Makefile /n x-release-static-php LIBS_CLI="%LIBS_CLI%"
-:: nmake /E /n /f Makefile  x-release-static-php mode=static
+nmake /E /f Makefile /n x-release-static-php
+:: nmake /E /n /f Makefile  x-release-static-php mode=static LIBS_CLI="%LIBS_CLI%"
 
+rem debug
+set "PATH=%PATH%;C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\"
+:: $env:Path += ";C:\Program Files (x86)\Windows Kits\10\Debuggers\x64"
 
+cdb.exe -pn ".\var\native-build\php-src\x64\Release_TS\php.exe"
 
 .\x64\Release_TS\php.exe -v
 .\x64\Release_TS\php.exe -m
