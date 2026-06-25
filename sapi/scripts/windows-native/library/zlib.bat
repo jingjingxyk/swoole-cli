166281c6cf8adc615445eec77e400d5793a64290
jingjingxyk <zonghengbaihe521@qq.com>
2026-06-25 18:07:41 +0800

    update

--- sapi/scripts/windows-native/library/zlib.bat
+++ sapi/scripts/windows-native/library/zlib.bat
@@ -42,9 +42,9 @@ cmake .. ^
 
 cmake --build . --config Release --target install
 
-del "%__PROJECT__%\build\zlib\lib\zlib.lib"
-del "%__PROJECT__%\build\zlib\lib\zlib.dll"
-copy /Y "%__PROJECT__%\build\zlib\lib\zlibstatic.lib" "%__PROJECT__%\build\zlib\lib\zlib.lib"
+rem del "%__PROJECT__%\build\zlib\lib\zlib.lib"
+rem del "%__PROJECT__%\build\zlib\bin\zlib.dll"
+rem copy /Y "%__PROJECT__%\build\zlib\lib\zlibstatic.lib" "%__PROJECT__%\build\zlib\lib\zlib.lib"
 dir "%__PROJECT__%\build\zlib\lib\"
 cd /d %__PROJECT__%
 endlocal
