760129fc8d9e1cc6a178a39ad8cf98e3e0f043f9
jingjingxyk <zonghengbaihe521@qq.com>
2026-06-28 10:45:29 +0800

    update

--- sapi/scripts/windows-native/library/openssl.bat
+++ sapi/scripts/windows-native/library/openssl.bat
@@ -30,16 +30,16 @@ perl -v
 
 
 set "INCLUDE=%cd%\include\;%cd%\apps\include\;%INCLUDE%"
+set CL=/MP
 :: perl apps/progs.pl -H apps/openssl > apps/progs.h
 perl Configure VC-WIN64A threads no-shared  no-legacy  no-tests  --release --prefix="%__PROJECT__%\build\openssl"  --openssldir="%__PROJECT__%\build\openssl\ssl"
 
-:: set CL=/MP
 
 rem document
 rem openssl\Configurations\windows-makefile.tmpl
 
 nmake
-nmake install
+nmake install_sw
 
 rem fix no found file " openssl/applink.c "
 rem copy %__PROJECT__%\thirdparty\openssl\openssl-3.6.0\ms\applink.c  %__PROJECT__%\build\openssl\include\openssl\applink.c
