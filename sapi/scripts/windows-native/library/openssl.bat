040106a9b4991ecf2e2abc96304716061934c317
jingjingxyk <zonghengbaihe521@qq.com>
2026-06-28 10:06:16 +0800

    update

--- sapi/scripts/windows-native/library/openssl.bat
+++ sapi/scripts/windows-native/library/openssl.bat
@@ -33,7 +33,7 @@ set "INCLUDE=%cd%\include\;%cd%\apps\include\;%INCLUDE%"
 :: perl apps/progs.pl -H apps/openssl > apps/progs.h
 perl Configure VC-WIN64A threads no-shared  no-legacy  no-tests  --release --prefix="%__PROJECT__%\build\openssl"  --openssldir="%__PROJECT__%\build\openssl\ssl"
 
-set CL=/MP
+:: set CL=/MP
 
 rem document
 rem openssl\Configurations\windows-makefile.tmpl
