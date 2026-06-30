9d058db5e633b582d1510736a37b6e0264f21cc8
jingjingxyk <zonghengbaihe521@qq.com>
2026-06-30 23:34:48 +0800

    update

--- sapi/scripts/windows-native/library/icu.bat
+++ sapi/scripts/windows-native/library/icu.bat
@@ -45,7 +45,7 @@ cd %__PROJECT__%\thirdparty\icu\icu\
 :: exit /b 0
 set "CPPFLAGS=-D U_CHARSET_IS_UTF8=1  -D U_USING_ICU_NAMESPACE=1  -D U_STATIC_IMPLEMENTATION=1 "
 
-bash ./source/runConfigureICU CYGWIN/MSVC --prefix=/cygdrive/d/a/swoole-cli/swoole-cli/build/icu/ ^
+bash ./source/runConfigureICU Cygwin/MSVC --prefix=/cygdrive/d/a/swoole-cli/swoole-cli/build/icu/ ^
 --enable-static=yes ^
 --enable-shared=no ^
 --with-data-packaging=static ^
