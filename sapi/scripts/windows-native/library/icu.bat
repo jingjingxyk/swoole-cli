09dc8c2b667a522605d17e336ef155b67b694664
jingjingxyk <zonghengbaihe521@qq.com>
2026-06-30 20:48:13 +0800

    update

--- sapi/scripts/windows-native/library/icu.bat
+++ sapi/scripts/windows-native/library/icu.bat
@@ -27,9 +27,7 @@ dir
 
 set "ORIGIN_PATH=%PATH%"
 set "PATH=%__PROJECT__%\runtime\nasm\;"
-set "PATH=%ProgramFiles%\7-Zip;%PATH%;"
 set "PATH=%__PROJECT__%\var\native-build\php-sdk-binary-tools\bin\;%__PROJECT__%\var\native-build\php-sdk-binary-tools\msys2\bin;%PATH%;"
-set "PATH=C:\Strawberry\perl\bin;%PATH%;"
 set "PATH=%PATH%;%ORIGIN_PATH%"
 echo "%PATH%"
 
