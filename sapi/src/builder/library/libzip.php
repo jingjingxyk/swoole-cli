f91ba7119390ed2732b2b41efa8dec641f00acbf
jingjingxyk <zonghengbaihe521@qq.com>
2026-07-05 16:01:55 +0800

    update

--- sapi/src/builder/library/libzip.php
+++ sapi/src/builder/library/libzip.php
@@ -14,8 +14,8 @@ return function (Preprocessor $p) {
         (new Library('libzip'))
             ->withHomePage('https://libzip.org/')
             ->withLicense('https://libzip.org/license/', Library::LICENSE_BSD)
-            ->withUrl('https://libzip.org/download/libzip-1.11.4.tar.gz')
-            ->withFileHash('md5', 'd28a3340febfb7fcf5d760895a4cab01')
+            ->withUrl('https://libzip.org/download/libzip-1.9.2.tar.gz')
+            ->withFileHash('md5', '345a88add7e9dd58aa029ac5b5b361ad')
             ->withManual('https://libzip.org')
             ->withPrefix($libzip_prefix)
             ->withConfigure(
