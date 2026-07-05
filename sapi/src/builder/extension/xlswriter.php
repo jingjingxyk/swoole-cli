279f108e13b4bc3dd283bb3210350fdf81fd3a2c
jingjingxyk <zonghengbaihe521@qq.com>
2026-07-05 17:26:51 +0800

    update

--- sapi/src/builder/extension/xlswriter.php
+++ sapi/src/builder/extension/xlswriter.php
@@ -9,8 +9,8 @@ return function (Preprocessor $p) {
         (new Extension('xlswriter'))
             ->withHomePage('https://github.com/viest/php-ext-xlswriter')
             ->withLicense('https://github.com/viest/php-ext-xlswriter/blob/master/LICENSE', Extension::LICENSE_BSD)
-            ->withPeclVersion('v3.0.0')
-            //->withFileHash('md5', '233f380e7fa2f74eb594946515fda1ad')
+            ->withPeclVersion('2.0.3')
+            ->withFileHash('md5', '233f380e7fa2f74eb594946515fda1ad')
             ->withOptions(' --with-xlswriter --enable-reader --with-openssl=' . OPENSSL_PREFIX)
             ->withDependentLibraries('openssl')
     );
