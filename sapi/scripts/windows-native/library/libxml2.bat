5e2d54e4fa298a9e516a4495595e9e57c18c039b
jingjingxyk <zonghengbaihe521@qq.com>
2026-07-06 19:46:24 +0800

    update

--- sapi/scripts/windows-native/library/libxml2.bat
+++ sapi/scripts/windows-native/library/libxml2.bat
@@ -33,7 +33,7 @@ cmake -S .. -B . ^
 -DBUILD_SHARED_LIBS=OFF ^
 -DCMAKE_BUILD_TYPE=Release ^
 -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded ^
--DIconv_DIR=C:\vcpkg\packages\libiconv_x64-windows-static\ ^
+-DIconv_ROOT=C:\vcpkg\packages\libiconv_x64-windows-static\ ^
 -DLIBXML_STATIC=ON ^
 -DLIBXML2_WITH_ZLIB=OFF ^
 -DLIBXML2_WITH_LZMA=OFF ^
