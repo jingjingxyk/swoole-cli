80ec162c5e8ca3fab4caf4ebcfab57e51d0e5b8a
jingjingxyk <zonghengbaihe521@qq.com>
2026-07-07 14:32:06 +0800

    update

--- sapi/scripts/windows-native/config.bat
+++ sapi/scripts/windows-native/config.bat
@@ -107,7 +107,7 @@ mklink /H %__PROJECT__%\build\openssl\lib\libeay32st.lib %__PROJECT__%\build\ope
 set CL=/MP
 
 configure.bat ^
---with-php-build=%__PROJECT__%\build ^
+--with-php-build="%__PROJECT__%\build" ^
 --with-extra-includes="%INCLUDE%" ^
 --with-extra-libs="%LIB%" ^
 --with-toolset=vs ^
