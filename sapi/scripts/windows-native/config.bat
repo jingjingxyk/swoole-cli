98ba8b714e4d9084df4d072a88a0d1819a84109f
jingjingxyk <zonghengbaihe521@qq.com>
2026-07-17 21:05:25 +0800

    update

--- sapi/scripts/windows-native/config.bat
+++ sapi/scripts/windows-native/config.bat
@@ -142,8 +142,9 @@ configure.bat ^
 --enable-phar-native-ssl ^
 --with-openssl=static ^
 --with-iconv=static ^
---enable-zip
-:: --with-bz2
+--with-mysqlnd ^
+--enable-zip ^
+--with-bz2
 
 :: --with-libxml=static ^
 :: --with-xml ^
@@ -157,7 +158,6 @@ configure.bat ^
 :: --enable-mbregex ^
 :: --enable-fileinfo ^
 :: --enable-intl ^
-:: --with-mysqlnd ^
 :: --enable-soap ^
 :: --with-xsl
 
