6e3903d33d938336c99953d44b509d2a718afa3f
jingjingxyk <zonghengbaihe521@qq.com>
2026-07-21 11:15:58 +0800

    update

--- sapi/scripts/windows-native/library/libzstd.bat
+++ sapi/scripts/windows-native/library/libzstd.bat
@@ -55,5 +55,7 @@ cmake --build . --config Release --target install
 del "%__PROJECT__%\build\libzstd\bin\zstd.dll"
 del "%__PROJECT__%\build\libzstd\lib\zstd.lib"
 copy /Y "%__PROJECT__%\build\libzstd\lib\zstd_static.lib" "%__PROJECT__%\build\libzstd\lib\zstd.lib"
+copy /Y "%__PROJECT__%\build\libzstd\lib\zstd.lib" "%__PROJECT__%\build\libzstd\lib\zstd_a.lib"
+copy /Y "%__PROJECT__%\build\libzstd\lib\zstd.lib" "%__PROJECT__%\build\libzstd\lib\libzstd_a.lib"
 cd /d %__PROJECT__%
 endlocal
