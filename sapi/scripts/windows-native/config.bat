2bc5001eafa3a813b7dbd8d1feb2f8f1756b1d2c
jingjingxyk <zonghengbaihe521@qq.com>
2026-07-25 01:25:28 +0800

    update

--- sapi/scripts/windows-native/config.bat
+++ sapi/scripts/windows-native/config.bat
@@ -131,7 +131,7 @@ set CL=/MP
 
 
 configure.bat ^
---with-php-build="%__PROJECT__%\\build" ^
+--with-php-build="%PHP_PHP_BUILD%" ^
 --with-extra-includes="%INCLUDE%" ^
 --with-extra-libs="%LIB%" ^
 --with-toolset=vs ^
