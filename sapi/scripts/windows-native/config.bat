8c1937652f9f2b7df0d4a6545bad56236e3f3524
jingjingxyk <zonghengbaihe521@qq.com>
2026-07-28 19:56:18 +0800

    update

--- sapi/scripts/windows-native/config.bat
+++ sapi/scripts/windows-native/config.bat
@@ -81,7 +81,7 @@ echo "========HELP============"
 
 call configure.bat --help
 
-sed.exe -i.".bak" '/hello/i STDOUT.WriteLine(PHP_PHP_BUILD);' configure.js
+sed.exe -i.".bak" '/ARG_WITH("verbosity"/i STDOUT.WriteLine(PHP_PHP_BUILD);' configure.js
 
 echo "===================="
 
