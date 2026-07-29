ba2239b4f97ea4ae374f85ee17a54d9d463b7e8f
jingjingxyk <zonghengbaihe521@qq.com>
2026-07-29 10:05:08 +0800

    update

--- sapi/scripts/windows-native/detect_sdk.bat
+++ sapi/scripts/windows-native/detect_sdk.bat
@@ -1,15 +1,15 @@
 @echo off
 :: chcp 65001 >nul
-chcp 936 >nul
+:: chcp 936 >nul
 for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -legacy -property resolvedInstallationPath`) do (
   set VSinstallDir=%%i
 )
-echo Visual Studio 安装路径: %VSinstallDir%
+echo Visual Studio installDir: %VSinstallDir%
 
 set VSCMD_ARG_HOST_ARCH=x64
 set VSCMD_ARG_TGT_ARCH=x64
 
 call %VSinstallDir%\Common7\Tools\vsdevcmd\core\winsdk.bat
 
-echo 默认 Windows SDK 版本: %WindowsSDKVersion%
-echo Windows SDK 库版本: %WindowsSDKLibVersion%
+echo default Windows SDK version: %WindowsSDKVersion%
+echo Windows SDK library version: %WindowsSDKLibVersion%
