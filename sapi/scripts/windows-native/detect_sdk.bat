1355bc7637043c26a912a662e8ab5b9e5b033d66
jingjingxyk <zonghengbaihe521@qq.com>
2026-07-29 10:06:19 +0800

    update

--- sapi/scripts/windows-native/detect_sdk.bat
+++ sapi/scripts/windows-native/detect_sdk.bat
@@ -1,7 +1,7 @@
 @echo off
 :: chcp 65001 >nul
 :: chcp 936 >nul
-for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -legacy -property resolvedInstallationPath`) do (
+for /f "usebackq tokens=*" %%i in (`vswhere.exe -latest -legacy -property resolvedInstallationPath`) do (
   set VSinstallDir=%%i
 )
 echo Visual Studio installDir: %VSinstallDir%
