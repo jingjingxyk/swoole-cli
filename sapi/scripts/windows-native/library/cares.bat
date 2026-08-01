0b7caeecbf804deec5e6cfbd4c36e33e25dd0609
jingjingxyk <zonghengbaihe521@qq.com>
2026-08-01 23:17:53 +0800

    update

--- sapi/scripts/windows-native/library/cares.bat
+++ sapi/scripts/windows-native/library/cares.bat
@@ -14,14 +14,14 @@ mkdir  build
 set CMAKE_BUILD_PARALLEL_LEVEL=%NUMBER_OF_PROCESSORS%
 
 cd /d %__PROJECT__%\pool\lib\
-7z.exe x -aoa -y   %__PROJECT__%\pool\lib\c-ares-1.24.0.tar.gz
+7z.exe x -aoa -y   "%__PROJECT__%\pool\lib\c-ares-1.24.0.tar.gz"
 
 if  exist "%__PROJECT__%\thirdparty\cares" rmdir /s /q "%__PROJECT__%\thirdparty\cares"
 mkdir "%__PROJECT__%\thirdparty\cares"
 
-7z.exe x -aoa -y  -o"%__PROJECT__%\thirdparty\cares"  %__PROJECT__%\pool\lib\c-ares-1.24.0.tar
+7z.exe x -aoa -y  -o"%__PROJECT__%\thirdparty\cares"  "%__PROJECT__%\pool\lib\c-ares-1.24.0.tar"
 
-cd /d %__PROJECT__%\thirdparty\cares\c-ares-1.24.0\
+cd /d "%__PROJECT__%\thirdparty\cares\c-ares-1.24.0"
 
 
 dir
@@ -45,13 +45,13 @@ cmake .. ^
 -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded ^
 -DBUILD_SHARED_LIBS=OFF  ^
 -DBUILD_STATIC_LIBS=ON ^
+-DCARES_MSVC_STATIC_RUNTIME=ON ^
 -DCARES_STATIC=ON ^
 -DCARES_SHARED=OFF ^
 -DCARES_STATIC_PIC=ON ^
 -DCARES_BUILD_TESTS=OFF ^
 -DCARES_BUILD_CONTAINER_TESTS=OFF ^
 -DCARES_BUILD_TOOLS=OFF ^
--DCARES_MSVC_STATIC_RUNTIME=ON ^
 -DCMAKE_C_FLAGS="/MT /O2 /W3  " ^
 -DCMAKE_EXE_LINKER_FLAGS="/VERBOSE:LIB" ^
 -DCMAKE_VERBOSE_MAKEFILE=ON
