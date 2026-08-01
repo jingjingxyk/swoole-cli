b71f69046da69845d21a4fb156012892dc986db1
jingjingxyk <zonghengbaihe521@qq.com>
2026-08-01 14:16:01 +0800

    update

--- sapi/scripts/windows-native/library/curl.bat
+++ sapi/scripts/windows-native/library/curl.bat
@@ -44,7 +44,7 @@ cmake .. ^
 -DCURL_USE_MBEDTLS=OFF ^
 -DENABLE_WEBSOCKETS=OFF ^
 -DCURL_USE_LIBSSH2=ON ^
--DUSE_NGHTTP2=ON ^
+-DCURL_USE_NGHTTP2=ON ^
 -DZSTD_LIBRARY=%__PROJECT__%\build\libzstd\lib\libzstd_a.lib ^
 -DZSTD_include=%__PROJECT__%\build\libzstd\include ^
 -DCMAKE_PREFIX_PATH="%__PROJECT__%\build\openssl;%__PROJECT__%\build\zlib;%__PROJECT__%\build\libssh2;%__PROJECT__%\build\brotli;%__PROJECT__%\build\libzstd;%LIBNGHTTP2_PREFIX%;"
