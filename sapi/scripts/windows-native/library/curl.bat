05b2a9b20699abdd412ab2ed5624537f2d99dae9
jingjingxyk <zonghengbaihe521@qq.com>
2026-08-03 17:43:49 +0800

    update

--- sapi/scripts/windows-native/library/curl.bat
+++ sapi/scripts/windows-native/library/curl.bat
@@ -88,7 +88,7 @@ cmake --build . --config Release --target install
 
 copy /Y %__PROJECT__%\build\curl\lib\libcurl.lib "%__PROJECT__%\build\curl\lib\libcurl_a.lib"
 
-dumpbin /DIRECTIVES "%__PROJECT__%\build\curl\lib\libcurl.lib" | findstr /i "DEFAULTLIB"
+dumpbin /DIRECTIVES "%__PROJECT__%\build\lib\libcurl.lib" | findstr /i "DEFAULTLIB"
 
 cd /d %__PROJECT__%
 endlocal
