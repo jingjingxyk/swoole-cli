bba534e19f2c036cc7d5fe8f67dd029d191757d5
jingjingxyk <zonghengbaihe521@qq.com>
2026-08-03 22:33:01 +0800

    update

--- sapi/scripts/windows-native/library/icu.bat
+++ sapi/scripts/windows-native/library/icu.bat
@@ -6,9 +6,7 @@ echo %~dp0
 cd %~dp0
 cd ..\..\..\..\
 
-rem
-rem cmd /c var\native-build\php-sdk-binary-tools\phpsdk-starter.bat -c vs17 -a x64  -t .\sapi\scripts\windows-native\library\icu.bat
-rem
+
 set __PROJECT__=%cd%
 cd /d %__PROJECT__%
 mkdir  %__PROJECT__%\build\icu\
@@ -28,7 +26,7 @@ dir
 set "ORIGIN_PATH=%PATH%"
 set "PATH=%__PROJECT__%\runtime\nasm\;"
 set "PATH=C:\cygwin64\bin\;%PATH%;"
-set "PATH=%__PROJECT__%\var\native-build\php-sdk-binary-tools\bin\;%__PROJECT__%\var\native-build\php-sdk-binary-tools\msys2\bin;%PATH%;"
+:: set "PATH=%__PROJECT__%\var\native-build\php-sdk-binary-tools\bin\;%__PROJECT__%\var\native-build\php-sdk-binary-tools\msys2\bin;%PATH%;"
 set "PATH=%PATH%;%ORIGIN_PATH%"
 echo "%PATH%"
 
@@ -50,12 +48,16 @@ if exist "C:\cygwin64\bin\link.exe" (
 cd %__PROJECT__%\thirdparty\icu\icu\
 
 :: exit /b 0
-set "CPPFLAGS=-D U_CHARSET_IS_UTF8=1  -D U_USING_ICU_NAMESPACE=1  -D U_STATIC_IMPLEMENTATION=1 "
+
 :: set "CXXFLAGS=/MT"
 :: set "CFLAGS=/EHsc /MP /MT /UCRT"
+set "CFLAGS=/EHsc /MP /MT"
+set "CXXFLAGS=/MT"
 
-sed.exe -i.".bak" "281 s/-Gy -MD/-Gy -MT/" .\source\runConfigureICU
-sed.exe -i.".bak" "282 s/-Gy -MD/-Gy -MT/" .\source\runConfigureICU
+set "CPPFLAGS=-D U_CHARSET_IS_UTF8=1  -D U_USING_ICU_NAMESPACE=1  -D U_STATIC_IMPLEMENTATION=1 "
+
+sed.exe -i.".bak" "s/RELEASE_CFLAGS='-Gy -MD'/RELEASE_CFLAGS='-Gy -MT'/" .\source\runConfigureICU
+sed.exe -i.".bak" "s/RELEASE_CXXFLAGS='-Gy -MD'/RELEASE_CXXFLAGS='-Gy -MT'/" .\source\runConfigureICU
 
 bash ./source/runConfigureICU Cygwin/MSVC --prefix=/cygdrive/d/a/swoole-cli/swoole-cli/build/icu/ ^
 --enable-static=yes ^
@@ -65,7 +67,7 @@ bash ./source/runConfigureICU Cygwin/MSVC --prefix=/cygdrive/d/a/swoole-cli/swoo
 --enable-extras=yes ^
 --enable-icuio=yes ^
 --enable-dyload=no ^
---enable-tools=yes ^
+--enable-tools=no ^
 --enable-tests=no ^
 --enable-samples=no
 
