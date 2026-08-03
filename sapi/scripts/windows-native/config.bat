f75d906aeca1ced68b658f8e37ca7a8a45983f02
jingjingxyk <zonghengbaihe521@qq.com>
2026-08-03 18:41:14 +0800

    update

--- sapi/scripts/windows-native/config.bat
+++ sapi/scripts/windows-native/config.bat
@@ -134,7 +134,7 @@ sed.exe -i.".bak" '/var i, j, k, libname;/i STDOUT.WriteLine(common_name);' conf
 
 sed.exe -i.".bak" '/generate_files();/i STDOUT.WriteLine("=======================");' configure.js
 sed.exe -i.".bak" '/generate_files();/i STDOUT.WriteLine(PHP_PHP_BUILD);' configure.js
-:: sed.exe -i.".bak" 's/CHECK_LIB("wldap32.lib", "curl", PHP_CURL)/1/' ext/curl/config.w32
+
 
 echo "===================="
 
@@ -199,24 +199,24 @@ configure.bat ^
 --enable-filter ^
 --enable-session ^
 --enable-tokenizer ^
---with-iconv=static ^
---with-libxml ^
---with-xml ^
---enable-xmlreader ^
---enable-xmlwriter ^
---with-dom ^
---with-simplexml ^
 --enable-ctype     --enable-pdo    --enable-phar  ^
 --enable-sockets ^
---enable-bcmath ^
 --enable-zlib ^
---enable-phar-native-ssl ^
---with-openssl=static ^
---with-mysqlnd ^
---with-bz2 ^
---enable-zip ^
 --with-curl
 
+:: --enable-phar-native-ssl ^
+:: --with-openssl=static ^
+:: --with-mysqlnd ^
+:: --with-bz2 ^
+:: --enable-zip ^
+:: --enable-bcmath ^
+:: --with-iconv=static ^
+:: --with-libxml ^
+:: --with-xml ^
+:: --enable-xmlreader ^
+:: --enable-xmlwriter ^
+:: --with-dom ^
+:: --with-simplexml ^
 :: --with-readline ^
 :: --with-gd ^
 :: --with-libwebp ^
