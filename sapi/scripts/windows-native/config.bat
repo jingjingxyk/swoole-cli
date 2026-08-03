c7e9896afbea62d937b7040b81669edd8b993647
jingjingxyk <zonghengbaihe521@qq.com>
2026-08-03 20:17:28 +0800

    update

--- sapi/scripts/windows-native/config.bat
+++ sapi/scripts/windows-native/config.bat
@@ -137,7 +137,7 @@ sed.exe -i.".bak" '/var i, j, k, libname;/i STDOUT.WriteLine(common_name);' conf
 sed.exe -i.".bak" '/generate_files();/i STDOUT.WriteLine("=======================");' configure.js
 sed.exe -i.".bak" '/generate_files();/i STDOUT.WriteLine(PHP_PHP_BUILD);' configure.js
 
-sed.exe -i.".bak" "s/php_libmagic.c', true/php_libmagic.c', null/" ext/fileinfo/config.w32
+sed.exe -i.".bak" "s/php_libmagic.c', true/php_libmagic.c',  /" ext/fileinfo/config.w32
 
 
 echo "===================="
